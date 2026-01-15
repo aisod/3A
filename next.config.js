4594491d6b1a484e44c673bf2f40ee083f000a00
AISOD Institute <aisodinstitute@gmail.com>
2026-01-15 02:45:23 +0200

    Fix API routes for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Ensure API routes work on Netlify
+  distDir: '.next',
 }
 
 module.exports = nextConfig
